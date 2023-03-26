8adb0234afdcd00dc9069405bb263ae413c59670
JhonataCustodio <silvajhonata@live.com>
2023-03-26 19:33:45 +0044

    created method in interface getByCpf

--- src/main/java/uol/compass/msclient/infra/repository/ClientRepository.java
+++ src/main/java/uol/compass/msclient/infra/repository/ClientRepository.java
@@ -3,5 +3,8 @@ package uol.compass.msclient.infra.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uol.compass.msclient.domain.Client;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByCpf(String cpf);
 }
