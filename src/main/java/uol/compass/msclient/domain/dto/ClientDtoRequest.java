275605430b8349d1cfc56fc21ba55f04c0829499
JhonataCustodio <silvajhonata@live.com>
2023-03-27 20:39:55 +0044

    create class dto client

---
+++ src/main/java/uol/compass/msclient/domain/dto/ClientDtoRequest.java
@@ -0,0 +1,16 @@
+package uol.compass.msclient.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientDtoRequest {
+    private String cpf;
+    private  String nome;
+    private Integer idade;
+}
