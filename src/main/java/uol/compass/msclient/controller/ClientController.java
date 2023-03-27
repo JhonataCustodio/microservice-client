dd752e1e877f2f4e1143339317cd8c0be163b68e
JhonataCustodio <silvajhonata@live.com>
2023-03-27 20:40:49 +0044

    create and point save client

--- src/main/java/uol/compass/msclient/controller/ClientController.java
+++ src/main/java/uol/compass/msclient/controller/ClientController.java
@@ -1,14 +1,24 @@
 package uol.compass.msclient.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uol.compass.msclient.domain.dto.ClientDtoRequest;
+import uol.compass.msclient.domain.dto.ClientDtoResponse;
+import uol.compass.msclient.service.ClientService;
 
 @RestController
 @RequestMapping("clients")
 public class ClientController {
+    @Autowired
+    private ClientService clientService;
     @GetMapping
     public String status(){
         return "ok";
+    }
+    @PostMapping()
+    public ResponseEntity<ClientDtoResponse> save(@RequestBody ClientDtoRequest request){
+        ClientDtoResponse clientDtoResponse = clientService.save(request);
+        return ResponseEntity.ok(clientDtoResponse);
     }
 }
