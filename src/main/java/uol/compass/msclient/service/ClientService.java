c15893061421597df4dfe7de7c26d401e6ac2139
JhonataCustodio <silvajhonata@live.com>
2023-03-27 20:39:26 +0044

    refactored the save method

--- src/main/java/uol/compass/msclient/service/ClientService.java
+++ src/main/java/uol/compass/msclient/service/ClientService.java
@@ -2,8 +2,12 @@ package uol.compass.msclient.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uol.compass.msclient.domain.Client;
+import uol.compass.msclient.domain.dto.ClientDtoRequest;
+import uol.compass.msclient.domain.dto.ClientDtoResponse;
+import uol.compass.msclient.domain.entity.Client;
 import uol.compass.msclient.infra.repository.ClientRepository;
 
 import java.util.Optional;
@@ -11,10 +15,16 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class ClientService {
+    @Autowired
     private final ClientRepository clientRepository;
+    @Autowired
+    private ModelMapper modelMapper;
     @Transactional
-    public Client save(Client client){
-        return clientRepository.save(client);
+    public ClientDtoResponse save(ClientDtoRequest request){
+        Client client = modelMapper.map(request, Client.class);
+        clientRepository.save(client);
+        ClientDtoResponse clientDtoResponse = modelMapper.map(client, ClientDtoResponse.class);
+        return clientDtoResponse;
     }
     public Optional<Client> getByCPF(String cpf){
         return clientRepository.findByCpf(cpf);
