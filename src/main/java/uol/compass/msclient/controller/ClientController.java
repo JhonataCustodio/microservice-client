223cba2b5afedc21c1d27c1e58e7c9ccd35705fe
JhonataCustodio <silvajhonata@live.com>
2023-03-28 21:18:17 +0044

    refactor method save

--- src/main/java/uol/compass/msclient/controller/ClientController.java
+++ src/main/java/uol/compass/msclient/controller/ClientController.java
@@ -1,24 +1,35 @@
 package uol.compass.msclient.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uol.compass.msclient.domain.dto.ClientDtoRequest;
 import uol.compass.msclient.domain.dto.ClientDtoResponse;
+import uol.compass.msclient.domain.entity.Client;
 import uol.compass.msclient.service.ClientService;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("clients")
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientController {
     @Autowired
     private ClientService clientService;
-    @GetMapping
-    public String status(){
-        return "ok";
-    }
     @PostMapping()
-    public ResponseEntity<ClientDtoResponse> save(@RequestBody ClientDtoRequest request){
-        ClientDtoResponse clientDtoResponse = clientService.save(request);
-        return ResponseEntity.ok(clientDtoResponse);
+    public ResponseEntity<Void> save(@RequestBody ClientDtoRequest request) {
+        URI headerLocation = clientService.save(request);
+        return ResponseEntity.created(headerLocation).build();
+    }
+    @GetMapping(params = "cpf")
+    public ResponseEntity dataClient(@RequestParam("cpf") String cpf){
+       var client = clientService.getByCPF(cpf);
+       if (client.isEmpty()){
+           return ResponseEntity.notFound().build();
+       }
+       return ResponseEntity.ok(client);
     }
 }
--- src/main/java/uol/compass/msclient/service/ClientService.java
+++ src/main/java/uol/compass/msclient/service/ClientService.java
@@ -1,30 +1,42 @@
 package uol.compass.msclient.service;
 
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import uol.compass.msclient.domain.dto.ClientDtoRequest;
 import uol.compass.msclient.domain.dto.ClientDtoResponse;
 import uol.compass.msclient.domain.entity.Client;
 import uol.compass.msclient.infra.repository.ClientRepository;
 
+import java.net.URI;
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientService {
     @Autowired
-    private final ClientRepository clientRepository;
+    private ClientRepository clientRepository;
     @Autowired
     private ModelMapper modelMapper;
     @Transactional
-    public ClientDtoResponse save(ClientDtoRequest request){
+    public URI save(@RequestBody ClientDtoRequest request){
         Client client = modelMapper.map(request, Client.class);
         clientRepository.save(client);
-        ClientDtoResponse clientDtoResponse = modelMapper.map(client, ClientDtoResponse.class);
-        return clientDtoResponse;
+        URI headerLocation = ServletUriComponentsBuilder.fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(client.getCpf())
+                .toUri();
+        //ClientDtoResponse clientDtoResponse = modelMapper.map(client, ClientDtoResponse.class);
+
+        return headerLocation;
     }
     public Optional<Client> getByCPF(String cpf){
         return clientRepository.findByCpf(cpf);
